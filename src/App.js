be75ffe98a43e9cf12ca85fbfaa4828c036e5fc6
SammSuccess <nwabudesamuelc@gmail.com>
2022-05-17 12:06:39 +0100

    text to speech app

--- src/App.js
+++ src/App.js
@@ -5,19 +5,29 @@ import {
   VoiceOverOff,
   SettingsOutlined
 } from '@mui/icons-material';
+import { useSpeechSynthesis } from "react-speech-kit";
 import Dialog from './Dialog'
 
-function App() {
+const App = () => {
   const [showSpeechSettings, setShowSpeechSettings] = useState(false)
   const [highlightedText, setHighlightedText] = useState('')
+  const [voiceIndex, setVoiceIndex] = useState(null);
+  const [pitch, setPitch] = useState(1);
+  const [rate, setRate] = useState(1);
 
-  // function to get value of hightlighted text and save in state
+  const onEnd = () => {
+    setHighlightedText('')
+  }
+  const { speak, cancel, speaking, supported, voices } = useSpeechSynthesis({onEnd})
+
+  const voice = voices[voiceIndex] || null
+
   const handleTextSet = () => {
     const text = window.getSelection().toString()
     if(text !== '') setHighlightedText(text)
   }
 
-  // setting highlightedText state onmouseup when text has been highlighted
+  // setting highlightedText state when text has been highlighted
   useEffect(() => {
     document.addEventListener('mouseup', handleTextSet)
     return () => {
@@ -34,23 +44,72 @@ function App() {
         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
         when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
         It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with 
-        desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+        software like Aldus PageMaker including versions of Lorem Ipsum.
       </p>
 
-      <div className="speechMenu">
-        {true
-          ? <RecordVoiceOverOutlined />
-          : <VoiceOverOff />
-        }
-        <SettingsOutlined onClick={() => setShowSpeechSettings(true)}/>
-      </div>
-        
-      <Dialog open={showSpeechSettings} onClose={() => setShowSpeechSettings(false)}>
+      {supported && 
+        <div className="speechMenu">
+          {!speaking
+            ? <RecordVoiceOverOutlined onClick={() => speak({ text: highlightedText, voice, rate, pitch})}/>
+            : <VoiceOverOff onClick={cancel}/>
+          }
+          <SettingsOutlined onClick={() => setShowSpeechSettings(true)}/>
+        </div>
+      }
 
+      <Dialog open={showSpeechSettings} onClose={() => setShowSpeechSettings(false)}>
+        <div className='speechSettings'>
+          {/* VOices -- browser dependent */}
+          <select
+            name="voice"
+            value={voiceIndex || ''}
+            onChange={(e) => {
+              setVoiceIndex(e.target.value)
+            }}
+          >
+            {voices.map((option, index) => (
+              <option key={option.voiceURI} value={index}>
+                {`${option.lang} - ${option.name} ${ option.default ? '- Default' : ''}`}
+              </option>
+            ))}
+          </select>
+          <div className='rangeContainer'>
+            <div>
+              <label htmlFor="rate">Rate: </label>
+              <span>{rate}</span>
+            </div>
+            <input
+              type="range"
+              min="0.5"
+              max="2"
+              step="0.1"
+              value={rate}
+              onChange={(e) => {
+                setRate(e.target.value);
+              }}
+            />
+          </div>
+          <div className='rangeContainer'>
+            <div>
+              <label htmlFor="pitch">Pitch: </label>
+              <span>{pitch}</span>
+            </div>
+            <input
+              type="range"
+              min="0"
+              max="2"
+              step="0.1"
+              value={pitch}
+              id="pitch"
+              onChange={(event) => {
+                setPitch(event.target.value);
+              }}
+            />
+          </div>
+        </div>
       </Dialog>
-      
-    </div>
+    </div>    
   )
 }
 
